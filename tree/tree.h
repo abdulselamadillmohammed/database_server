4c8d3702eeb4e97398f4965f7e8ff1524f7753cf
Abdulselam Adill <forcoding39@gmail.com>
2026-02-03 20:56:29 -0500

    define node structure

--- tree/tree.h
+++ tree/tree.h
@@ -9,3 +9,10 @@
 
 #include <assert.h>
 #include <errno.h>
+
+// Node structure
+struct s_node {
+    struct s_node *north; // upward link, ie linked list
+    struct s_node *west;
+    struct s_leaf *east; // Notice datatype difference 
+}
