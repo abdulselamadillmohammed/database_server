2e6996e179412872af0d4d752917a9312e3f43e4
Abdulselam Adill <forcoding39@gmail.com>
2026-02-03 20:57:52 -0500

    define custom datatypes

--- tree/tree.h
+++ tree/tree.h
@@ -10,9 +10,14 @@
 #include <assert.h>
 #include <errno.h>
 
+
+typedef unsigned int int32;
+typedef unsigned short int int16;
+typedef unsigned char int8;
+
 // Node structure
 struct s_node {
     struct s_node *north; // upward link, ie linked list
     struct s_node *west;
     struct s_leaf *east; // Notice datatype difference 
-}
+};
