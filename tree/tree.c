ad1918658f926783da5ccc8daa60039502965735
Abdulselam Adill <forcoding39@gmail.com>
2026-02-04 23:34:10 -0500

    modify node creation mechanism

--- tree/tree.c
+++ tree/tree.c
@@ -45,6 +45,13 @@ Node *create_node(Node *parent, int8 *path){
     size = sizeof(struct s_node);
     n = (Node *)malloc(size);
     zero((int8 *)n, size);
+
+    parent->west = n;
+    n->tag = TagNode;
+    n->north = parent;
+    strncpy((char *)n->path, (char *)path,255);
+        
+    return n;
 }
 
 
